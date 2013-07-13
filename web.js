b9e04f9e85d5cddf16f770276418317b850cbfa3
Omar Aragon <ofasx1@gmail.com>
2013-07-13 02:47:02 +0000

    Data index.html

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync("index.html"); 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
